b1495c3329cd15a69008baa4a03f131b2dcefae7
Fabrizio Cirelli <fcirelli@sorint.it>
2019-12-10 15:34:13 +0100

    Initial vicook.js script

---
+++ vicook.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const cheerio = require('cheerio')
+
+request('https://grassobbio.vicookcloud.it/menu', function (error, response, body) {
+    const $ = cheerio.load(body)
+
+    $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
+        element = element.firstChild.data
+
+        console.log(element)
+    })
+});
+
