af211a6c4e0e34a2c06c12b15404f1cca3876de1
Andrea Bonadei <andrea.bonadei@sorint.it>
2019-12-10 17:54:55 +0100

    Capitalize first letter

--- vicook.js
+++ vicook.js
@@ -1,13 +1,14 @@
+"use strict";
+
 const request = require('request');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 
 request('https://grassobbio.vicookcloud.it/menu', function (error, response, body) {
-    const $ = cheerio.load(body)
+    const $ = cheerio.load(body);
 
     $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
-        element = element.firstChild.data
-
-        console.log(element)
+        element = element.firstChild.data;
+        console.log(element.charAt(0).toUpperCase() + element.substring(1));
     })
 });
 
