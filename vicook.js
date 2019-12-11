b4b2e201f6a2520ed5a9d1fd0367f9c7df03b440
Fabrizio Cirelli <cirelli94@gmail.com>
2019-12-11 18:10:22 +0100

    Added -t parameter but not working!

--- vicook.js
+++ vicook.js
@@ -2,20 +2,50 @@
 
 const request = require('request');
 const cheerio = require('cheerio');
+const program = require('commander');
+
+program.version("0.0.2");
+program.option('-t, --tomorrow', 'show tomorrow menu');
+
+program.parse(process.argv);
+
 
 request('https://grassobbio.vicookcloud.it/menu', function (error, response, body) {
     const $ = cheerio.load(body);
 
-    console.log("=== OGGI ===")
-    console.log(capitalize($('h2.second')[0].firstChild.data))
-    console.log("============")
+    if (!program.tomorrow) {
 
-    $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
-        element = element.firstChild.data;
-        console.log(capitalize(element))
-    })
+        logDate("OGGI", $);
+        logMenu($);
+
+    } else {
+        let domani = $('a.mono_day_selector')[0].href;
+
+        console.log(domani);
+
+        request(domani, function(error, response, body){
+            const $ = cheerio.load(body);
+
+            logDate("DOMANI", $);
+            logMenu($);
+       });
+    }
 });
 
 function capitalize(element){
     return element.charAt(0).toUpperCase() + element.substring(1);
-}
+};
+
+function logMenu($){
+    $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
+        element = element.firstChild.data;
+        console.log(capitalize(element))
+    });
+};
+
+function logDate(text, $){
+    console.log("=== " + text + " ===")
+    console.log(capitalize($('h2.second')[0].firstChild.data))
+    console.log("============")
+};
+
