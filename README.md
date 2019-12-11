1584902f4808304476c746c6fca6d5d0bff4883c
Andrea Bonadei <bonadei.andrea@gmail.com>
2019-12-11 20:54:36 +0100

    Added -t option to view tomorrow's menu

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 Sometimes you want to know what you'll eat for launch. Sometimes not.
 This is for when you want to know.
 
-## How-to
+## Install
 * [Install npm](https://lmgtfy.com/?q=how+to+install+npm)
 * Install with `npm i -g vicook`
 * Run `vicook`
+
+## Options
+* ` -t, --tomorrow ` show tomorrow menu
+* ` -V, --version `  output the version number
+* ` -h, --help `     output usage information
+
+
--- vicook.js
+++ vicook.js
@@ -1,51 +1,56 @@
 "use strict";
 
-const request = require('request');
-const cheerio = require('cheerio');
-const program = require('commander');
+const request = require("request");
+const cheerio = require("cheerio");
+const program = require("commander");
 
-program.version("0.0.2");
-program.option('-t, --tomorrow', 'show tomorrow menu');
+program.version("0.0.3");
+
+program.option("-t, --tomorrow", "show tomorrow menu");
 
 program.parse(process.argv);
 
 
-request('https://grassobbio.vicookcloud.it/menu', function (error, response, body) {
-    const $ = cheerio.load(body);
+const url = "https://grassobbio.vicookcloud.it/menu/";
+let epoch = getTodayDate().getTime() / 1000; //Today epoch in local timezone
 
-    if (!program.tomorrow) {
-
+if (!program.tomorrow) {
+    //Today
+    request(url + epoch, function (error, response, body) {
+        const $ = cheerio.load(body);
         logDate("OGGI", $);
         logMenu($);
-
-    } else {
-        let domani = $('a.mono_day_selector')[0].href;
-
-        console.log(domani);
-
-        request(domani, function(error, response, body){
-            const $ = cheerio.load(body);
-
-            logDate("DOMANI", $);
-            logMenu($);
-       });
-    }
-});
-
-function capitalize(element){
-    return element.charAt(0).toUpperCase() + element.substring(1);
-};
-
-function logMenu($){
-    $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
-        element = element.firstChild.data;
-        console.log(capitalize(element))
     });
-};
+} else {
+    // Tomorrow
+    epoch += (24 * 60 * 60);
+    request(url + epoch, function (error, response, body) {
+        const $ = cheerio.load(body);
+        logDate("DOMANI", $);
+        logMenu($);
+    });
+}
 
-function logDate(text, $){
+function logMenu($) {
+    $("div").find(".cbp-l-grid-masonry-projects-title").each(function (index, element) {
+        let menuEntry = element.firstChild.data;
+        console.log("\u2022 " + capitalizeFirstLetter(menuEntry))
+    });
+}
+
+function logDate(text, $) {
     console.log("=== " + text + " ===")
-    console.log(capitalize($('h2.second')[0].firstChild.data))
+    console.log(capitalizeFirstLetter($("h2.second")[0].firstChild.data))
     console.log("============")
-};
+}
+
+function getTodayDate() {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate());
+}
+
+function capitalizeFirstLetter(s) {
+    return s.charAt(0).toUpperCase() + s.substring(1);
+}
+
 
