5405f344c7218bbfa981d69da49ebb56267d185b
Fabrizio Cirelli <cirelli94@gmail.com>
2019-12-11 10:15:57 +0100

    Added today date, refactoring

--- vicook.js
+++ vicook.js
@@ -6,9 +6,16 @@ const cheerio = require('cheerio');
 request('https://grassobbio.vicookcloud.it/menu', function (error, response, body) {
     const $ = cheerio.load(body);
 
+    console.log("=== OGGI ===")
+    console.log(capitalize($('h2.second')[0].firstChild.data))
+    console.log("============")
+
     $('div').find('.cbp-l-grid-masonry-projects-title').each(function (index, element){
         element = element.firstChild.data;
-        console.log(element.charAt(0).toUpperCase() + element.substring(1));
+        console.log(capitalize(element))
     })
 });
 
+function capitalize(element){
+    return element.charAt(0).toUpperCase() + element.substring(1);
+}
